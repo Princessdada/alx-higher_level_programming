e50ece1c2f19a76719dbf42edc74f46faffad29d
princessdada <princessdada321@gmail.com>
2022-09-23 02:07:42 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+n = len(sys.argv)
+if __name__ == "__main__":
+if len(sys.argv) == 1:
+print("0 arguments.")
+elif len(sys.argv) == 2:
+print((len(sys.argv) - 1), "argument:")
+for x in range(1, n):
+print("{:d}:".format(x), sys.argv[x])
+else:
+print((len(sys.argv) - 1), "arguments:")
+for x in range(1, n):
+print("{:d}:".format(x), sys.argv[x])
