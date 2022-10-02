879376e6517ca767817c59e0fbc9f6d8c7818d8b
princessdada <princessdada321@gmail.com>
2022-10-02 13:38:26 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-if my_list is None:
-return None
-return [replace if x == search else x for x in my_list]
+
+new_list = list(map(lambda x: replace if x == search else x, my_list))
+
+return (new_list)
